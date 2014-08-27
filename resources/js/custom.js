cc14a446312dbbfc21558e2818610210bcbf19ae
Sibelious <sibelious@outlook.com>
2014-08-27 20:27:00 +0100

    added slider
    now working on the 3d...

--- resources/js/custom.js
+++ resources/js/custom.js
@@ -1,8 +1,19 @@
 $(document).ready(function(){
 $('#secondScreen').hide();
 
+	function startSwiper(){
+  	var mySwiper = $('.swiper-container').swiper({
+    //Your options here:
+    mode:'horizontal',
+    loop: true
+    //etc..
+  });
+  	
+}
+
 	$('#submitButton').click(function(){
 		$('#firstScreen').fadeOut( 500 );
 		$('#secondScreen').fadeIn( 500 );
+		startSwiper();
 	});
 });
